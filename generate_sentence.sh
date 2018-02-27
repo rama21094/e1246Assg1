3ffe0b1c2c079ad9889869b52123a96e9c3e0320
Shankararama Sharma R <shanku21094@gmail.com>
2018-02-28 02:20:42 +0530

    Create generate_sentence.sh

---
+++ generate_sentence.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo '5 sentences will be displayed.'
+python3 generate_sentence.py
+python3 generate_sentence.py
+python3 generate_sentence.py
+python3 generate_sentence.py
+python3 generate_sentence.py
